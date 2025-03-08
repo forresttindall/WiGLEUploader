71dadaef11013be6b09b03376acf8da31299223d
forresttindall <forrest.tindall@gmail.com>
2025-03-08 14:11:33 -0700

    about copy edit

--- src/components/Pages/AboutPage.js
+++ src/components/Pages/AboutPage.js
@@ -6,13 +6,14 @@ function AboutPage() {
       <h2>About WiGLE Uploader</h2>
       <p>WiGLE Uploader is an open source tool designed to help you upload your wardriving files faster to the WiGLE database.</p>
       <br></br>
-      <p>WiGLE Uploader features true zero-knowledge architecture and stores no data.</p>
+      <p>WiGLE Uploader features true privacy and stores no data.</p>
       <br></br>
       <ul>
         <li>Drag and drop file uploads</li>
         <li>Batch uploading of multiple files</li>
         <li>Support for all major wardriving file formats</li>
         <li>Simple and secure authentication</li>
+        <li>Badge Generator for sharing your WiGLE stats</li>
       </ul>
       <br></br>
       <h2>Created and maintained by <a href="https://4est.io" target="_blank" rel="noopener noreferrer">4est.io.</a> 

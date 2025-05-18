781af63b4d50a8fe617702b4aaafcecdd8ca9e74
forresttindall <forrest.tindall@gmail.com>
2025-05-18 11:56:49 -0600

    About page backlinks

--- src/components/Pages/AboutPage.js
+++ src/components/Pages/AboutPage.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import './AboutPage.css'
 
 function AboutPage() {
   return (
     <div className="page-content">
       <h2>About WiGLE Uploader</h2>
+      <br></br>
+      
+      <h2>Created and maintained by <a href="https://forresttindall.dev/links" target="_blank" rel="noopener noreferrer">Forrest Tindall</a> Of <a href="https://creationbase.io" target="_blank" className="cb-text"><img src="./dot-triangle.png" className="cb-logo" /><span className="cb-text">Creationbase</span></a></h2> 
+
       <p>WiGLE Uploader is an open source tool designed to help you upload your wardriving files faster to the WiGLE database.</p>
       <br></br>
       <p>WiGLE Uploader features true privacy and stores no data.</p>
@@ -16,10 +21,12 @@ function AboutPage() {
         <li>Badge Generator for sharing your WiGLE stats</li>
       </ul>
       <br></br>
-      <h2>Created and maintained by <a href="https://forresttindall.dev" target="_blank" rel="noopener noreferrer">Forrest Tindall</a> 
+      
       <br></br>
       <br></br>
-      Please support the project by <a href="https://account.venmo.com/u/ForrestTindall" target="_blank" rel="noopener noreferrer">donating here.</a></h2>
+      <h2>Please support the project by <a href="https://account.venmo.com/u/ForrestTindall" target="_blank" rel="noopener noreferrer">donating here.</a></h2>
+      <br></br>
+      <h2>Please Star the GitHub Repo. <a href="https://github.com/forresttindall/WiGLEUploader" target='_blank'>Here</a></h2>
     </div>
   );
 }
